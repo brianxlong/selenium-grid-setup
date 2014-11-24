2417c1b45ad1232649b1e02aad2186f050ac601d
brian long <brianx@vt.edu>
2014-11-24 16:23:56 -0500

    changes for version 1.0

--- grid-test/helpers/isgridup.sh
+++ grid-test/helpers/isgridup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-java -cp ~/apps/src/selenium-grid-setup/grid-test/target/grid-test-0.0.1-SNAPSHOT.jar \
+java -cp ~/apps/src/selenium-grid-setup/grid-test/target/grid-setup-test-1.0.jar \
 org.testng.TestNG \
 ~/apps/src/selenium-grid-setup/grid-test/src/main/resources/testng-grid.xml 
 
