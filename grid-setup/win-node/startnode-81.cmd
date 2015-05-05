48ffa62842ce21e894c933f82c5fbe2b660e1309
brianxlong <brian.long@vt.edu>
2015-05-05 14:22:17 -0400

    remove multiple links to standalone jar in favor of references and a single link (thanks, jason)

--- grid-setup/win-node/startnode-81.cmd
+++ grid-setup/win-node/startnode-81.cmd
@@ -1,2 +1,1 @@
-java -jar ..\selenium-server-standalone-2.45.0.jar -role node -nodeConfig \
-nodeconfig-81.json -Dwebdriver.firefox.profile=default
+java -jar ..\selenium-server-standalone-2.45.0.jar -role node -nodeConfig nodeconfig-81.json -Dwebdriver.firefox.profile=default
