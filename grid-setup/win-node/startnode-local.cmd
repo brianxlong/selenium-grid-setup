4d20a062742aca0dc366bc1d7311cddcbe607d92
Brian Long <brian.long@vt.edu>
2016-04-26 20:50:13 -0400

    updates to configs update dependencies in pom update RemoteTest version handling, add edge update testng-grid.xml

---
+++ grid-setup/win-node/startnode-local.cmd
@@ -0,0 +1,1 @@
+java -jar ..\selenium-server-standalone-2.46.0.jar -role node -nodeConfig nodeconfig-local.json -Dwebdriver.firefox.profile=default
---
+++ grid-test/helpers/isgridup.cmd
@@ -0,0 +1,2 @@
+java -cp ..\target\grid-setup-test-1.0.jar org.testng.TestNG ..\src\main\resources/testng-grid.xml 
+
